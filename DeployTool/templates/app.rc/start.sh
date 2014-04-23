3139999dc2f87aaca35c228a66df3390e6cdc999
Ilya N. Zykin <zykin-ilya@ya.ru>
2014-04-23 22:35:36 +0400

    init_commit

---
+++ DeployTool/templates/app.rc/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+RAILS_ROOT=<%= dest %>
+RVM_DO="<%= rvm_do %>"
+
+cd $RAILS_ROOT
+
+echo "try to start Redis"
+(redis-server ./config/<%= fetch :redis_config_file %>) || (echo "Redis can't be started")
+
+echo "try to start Sidekiq"
+($RVM_DO bundle exec sidekiq -e <%= fetch :stage %> -d -C ./config/sidekiq.yml) || (echo "Sidekiq can't be started")
+
+echo "try to start Sphinx"
+echo ""
+(RAILS_ENV=<%= fetch :stage %> $RVM_DO bundle exec rake ts:configure) || (echo "Sphinx can't be configated")
+(RAILS_ENV=<%= fetch :stage %> $RVM_DO bundle exec rake ts:index)     || (echo "Sphinx can't be indexed")
+(RAILS_ENV=<%= fetch :stage %> $RVM_DO bundle exec rake ts:start)     || (echo "Sphinx can't be started")
